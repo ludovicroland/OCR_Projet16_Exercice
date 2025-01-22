cc91730d16eca66cbb2e0eabb265cd0e6b960be6
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-22 12:10:42 +0100

    build(gradle.app): Adding release SignInConfig with generated signature keystore + Adding Jacoco configuration for the tests coverage (with exclusion).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,42 @@
+import com.android.build.gradle.BaseExtension
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     kotlin("kapt")
     id("com.google.dagger.hilt.android")
+    id("jacoco")
 }
 
+
+
+//Signature app
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
 android {
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
     namespace = "com.openclassrooms.arista"
     compileSdk = 34
+
+    testCoverage {
+        version = "0.8.8"
+    }
+
+    testOptions {
+        animationsDisabled = true
+        unitTests.isIncludeAndroidResources = true
+    }
 
 
     defaultConfig {
@@ -27,7 +56,13 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
+        debug {
+            enableAndroidTestCoverage = true
+            enableUnitTestCoverage = true
+        }
+
     }
     compileOptions {
         // Flag to enable support for the new language APIs
@@ -42,10 +77,19 @@ android {
     buildFeatures {
         viewBinding = true
     }
-    testOptions {
-        animationsDisabled = true
+
+
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
+
+
 }
+
+
 
 dependencies {
     val roomVersion = "2.6.0"
@@ -78,7 +122,63 @@ dependencies {
     androidTestImplementation("androidx.test:runner:1.6.0")
     androidTestImplementation("androidx.test:rules:1.6.0")
     implementation("androidx.tracing:tracing:1.2.0")
+    implementation(kotlin("script-runtime"))
+
+
 }
 kapt {
     correctErrorTypes = true
 }
+
+tasks.withType<Test> {
+    extensions.configure(JacocoTaskExtension::class) {
+        isIncludeNoLocationClasses = true
+        excludes = listOf("jdk.internal.*")
+    }
+}
+
+val androidExtension = extensions.getByType<BaseExtension>()
+
+val jacocoTestReport by tasks.registering(JacocoReport::class) {
+    dependsOn("testDebugUnitTest","connectedDebugAndroidTest", "createDebugCoverageReport")
+    group = "Reporting"
+    description = "Generate Jacoco coverage reports"
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+    val fileFilter = listOf(
+        "**/R.class",
+        "**/R$*.class",
+        "**/di/**",
+        "**/hilt/**",
+        "**/Hilt_*.*",
+        "**/com/openclassrooms/arista/ui/exercise/*$*.class",
+        "**/com/openclassrooms/arista/ui/sleep/*$*.class",
+        "**/com/openclassrooms/arista/ui/user/*$*.class",
+        "**/com/openclassrooms/arista/data/*$*.class",
+        "**/com/openclassrooms/arista/domain/model/*$*.class",
+
+    )
+
+    val kotlinDebugClassesDir = fileTree("${project.buildDir}/tmp/kotlin-classes/debug/") {
+        exclude(fileFilter)
+    }
+
+    val mainSrc = androidExtension.sourceSets.getByName("main").java.srcDirs
+
+    classDirectories.setFrom(kotlinDebugClassesDir)
+    sourceDirectories.setFrom(files(mainSrc))
+    executionData.setFrom(
+        fileTree(project.buildDir) {
+            include(
+                "**/*.exec", "**/*.ec"
+            )
+        }
+    )
+}
+
+
+
+
