a04c95599401ec909a6d91512646439ef0e89163
Samuel Lebailly <sam.meusa@gmail.com>
2025-01-28 22:32:51 +0100

    test(AndroidTest): - Add instrumented test of the main Activity.

--- app/src/androidTest/java/com/openclassrooms/arista/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/openclassrooms/arista/ui/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.openclassrooms.arista.ui
 
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -44,4 +45,37 @@ class MainActivityTest
     )
     editText2.check(matches(isDisplayed()))
   }
+
+
+
+  @Test
+  fun testInitialFragmentDisplayed() {
+    // Vérifie que le fragment UserDataFragment est affiché au démarrage
+    onView(withId(R.id.fragment_container)).check(matches(isDisplayed()))
+    onView(withId(R.id.et_name)).check(matches(isDisplayed()))
+    onView(withId(R.id.et_email)).check(matches(isDisplayed()))
+  }
+
+  @Test
+  fun testBottomNavigationNavigatesToExerciseFragment() {
+    // Clique sur le bouton Exercise dans la BottomNavigation
+    onView(withId(R.id.nav_exercise)).perform(click())
+
+    // Vérifie que le fragment ExerciseFragment est affiché
+    onView(withId(R.id.fragment_container)).check(matches(isDisplayed()))
+    onView(withId(R.id.exercise_recyclerview)).check(matches(isDisplayed()))
+  }
+
+  @Test
+  fun testBottomNavigationNavigatesToSleepFragment() {
+    // Clique sur le bouton Sleep dans la BottomNavigation
+    onView(withId(R.id.nav_sleep)).perform(click())
+
+    // Vérifie que le fragment SleepFragment est affiché
+    onView(withId(R.id.fragment_container)).check(matches(isDisplayed()))
+    onView(withId(R.id.sleep_recyclerview)).check(matches(isDisplayed())) // Exemple d'élément spécifique au fragment Sleep
+  }
+
+
+
 }
